f7ebe103da2a77723225f1cdfccab96865621256
Lucas Wingenfeld <lucaswingenfeld@gmail.com>
2026-08-28 00:40:19 +0200

    design

--- src/routes/telecare.tsx
+++ src/routes/telecare.tsx
@@ -140,7 +140,7 @@ function TeleCare() {
             </div>
             <div className="img-container" id="img-3">
               <img
-                src={getAssetUrl("media/team.png")}
+                src={getAssetUrl("media/woman.png")}
                 alt=""
                 id="img-womanThink"
                 className="intro-img"
