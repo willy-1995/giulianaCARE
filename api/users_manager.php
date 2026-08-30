cd84dd7682de91c02d0d7b94ea0fdf55aab3b020
Lucas Wingenfeld <lucaswingenfeld@gmail.com>
2026-08-30 20:15:20 +0200

    regist upd

--- api/users_manager.php
+++ api/users_manager.php
@@ -17,7 +17,7 @@ try {
         // CREATE (REGISTRIERUNG)
         // ==========================================
         case 'POST':
-            if (empty($input['email']) || empty($input['password']) || empty($input['role'])) {
+            if (empty($input['email']) || empty($input['password']) || empty($input['area_code']) || empty($input['country']) || empty($input['price'])) {
                 http_response_code(400);
                 echo json_encode(["success" => false, "message" => "Email, Passwort und Rolle sind Pflichtfelder."]);
                 exit;
